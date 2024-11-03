ceabe3ab77efbd605ff93d9b83d87cfc0149ad2a
Hemna <waboring@hemna.com>
2024-11-02 20:41:39 -0400

    Initial commit
    This commit is from running the initial KvDeveloper create

---
+++ View/screens.py
@@ -0,0 +1,24 @@
+# The screen's dictionary contains the objects of the models and controllers
+# of the screens of the application.
+
+from Model.home_screen import HomeScreenModel
+from Controller.home_screen import HomeScreenController
+from Model.login_screen import LoginScreenModel
+from Controller.login_screen import LoginScreenController
+from Model.settings_screen import SettingsScreenModel
+from Controller.settings_screen import SettingsScreenController
+
+screens = {
+    'settings screen': {
+        'model': SettingsScreenModel,
+        'controller': SettingsScreenController,
+    },
+    'home screen': {
+        'model': HomeScreenModel,
+        'controller': HomeScreenController,
+    },
+    'login screen': {
+        'model': LoginScreenModel,
+        'controller': LoginScreenController,
+    },
+}
